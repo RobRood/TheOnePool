e6581983b16febc7071eca65ab90046aef0c6055
RobRood <therobertrood@gmail.com>
2019-06-11 14:31:55 -0700

    Initial Commit
    ObjectPool Class
    TheOnePool MonoBehavior

---
+++ TheOnePool/ObjectPool.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UIElements; //needed for unity 2019, remove if using 2018. 2017 and earlier not tested
+using UnityEngine.UIElements; //needed for unity 2019, remove if using 2018. 2017 and earlier not tested
+
+//Code by Robert Rood, Originally made for use in Unity 2019.1, MIT License
+
+[System.Serializable]
+public class ObjectPool
+{
+
+    public GameObject pooledObject;
+    public int poolSize = 10;
+    public bool canResize = true;
+
+    public List<GameObject> pooledObjects;
+    public string poolName; //optional, allows for pool to be referenced by name, first pool with matching name will be used
+}
+
+
+[CustomPropertyDrawer(typeof(ObjectPool))]
+public class ObjectPoolDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        var nameField = new Rect(position.x - 40, position.y + 20, 150, 16);
+        var objectField = new Rect(position.x - 40, position.y + 40, 150, 16);
+        var sizeField = new Rect(position.x - 40, position.y + 60, 50, 16);
+        var resizeField = new Rect(position.x + 80, position.y + 60, 30, 16);
+
+        EditorGUI.LabelField(new Rect(nameField.x - 86, nameField.y, 80, nameField.height), new GUIContent("Pool Name"));
+        EditorGUI.LabelField(new Rect(objectField.x - 86, objectField.y, 96, objectField.height), new GUIContent("Pooled Object"));
+        EditorGUI.LabelField(new Rect(sizeField.x - 86, sizeField.y, 80, sizeField.height), new GUIContent("Pool Size"));
+        EditorGUI.LabelField(new Rect(resizeField.x - 68, resizeField.y, 80, resizeField.height), new GUIContent("Can Resize"));
+
+
+        EditorGUI.PropertyField(nameField, property.FindPropertyRelative("poolName"), GUIContent.none);
+        EditorGUI.PropertyField(objectField, property.FindPropertyRelative("pooledObject"), GUIContent.none);
+        EditorGUI.PropertyField(sizeField, property.FindPropertyRelative("poolSize"), GUIContent.none);
+        EditorGUI.PropertyField(resizeField, property.FindPropertyRelative("canResize"), GUIContent.none); 
+
+        EditorGUI.EndProperty();
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return 90;
+    }
+}
+
---
+++ TheOnePool/TheOnePool.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//"One Pool to rule them all, One pool to find them,
+//One Pool to bring them all and in the inspector bind them"
+//							-inspired by The One Ring Poem, j.R.R.Tolkin 
+
+//Code by Robert Rood, Originally made for use in Unity 2019.1, MIT License
+
+[DisallowMultipleComponent]
+public class TheOnePool : MonoBehaviour
+{
+    //Long term references
+    public static TheOnePool instance; // allow for the OnePool to be accessed in script without a reference;
+    [SerializeField] ObjectPool[] objectPools = new ObjectPool[1]; //default to at least one pool when attaching monobehavior 
+
+    // Initialize
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        OpenThePool();
+    }
+
+
+    private void OpenThePool()
+    {
+        for (int j = 0; j < objectPools.Length; j++)
+        {
+            objectPools[j].pooledObjects = new List<GameObject>();
+
+            for (int i = 0; i < objectPools[j].poolSize; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(objectPools[j].pooledObject);
+                obj.SetActive(false);
+                objectPools[j].pooledObjects.Add(obj);
+            }
+        }
+    }
+
+    //Intervals
+    //get objet
+    public GameObject GetObjectOutOfPool(int pool)
+    {
+        for (int i = 0; i < objectPools[pool].pooledObjects.Count; i++)
+        {
+            if (!objectPools[pool].pooledObjects[i].activeInHierarchy)
+            {
+                objectPools[pool].pooledObjects[i].SetActive(true);
+                return objectPools[pool].pooledObjects[i];
+            }
+        }
+
+        if (objectPools[pool].canResize)
+        {
+            GameObject obj = (GameObject)Instantiate(objectPools[pool].pooledObject);
+            objectPools[pool].pooledObjects.Add(obj);
+            objectPools[pool].poolSize++;
+            return obj;
+        }
+
+        return null;
+    }
+    //get object and set its tranform
+    public GameObject GetObjectOutOfPool(int pool, Vector3 pos, Quaternion rot)
+    {
+        for (int i = 0; i < objectPools[pool].pooledObjects.Count; i++)
+        {
+            if (!objectPools[pool].pooledObjects[i].activeInHierarchy)
+            {
+                GameObject obj = objectPools[pool].pooledObjects[i];
+                obj.transform.position = pos;
+                obj.transform.rotation = rot;
+                obj.SetActive(true);
+                return obj;
+            }
+        }
+        if (objectPools[pool].canResize)
+        {
+            GameObject obj = Instantiate(objectPools[pool].pooledObject, pos, rot);
+            objectPools[pool].pooledObjects.Add(obj);
+            objectPools[pool].poolSize++;
+            return obj;
+        }
+        return null;
+    }
+
+    //get object by pool name and set its transform
+    public GameObject GetObjectOutOfPool(string poolName, Vector3 pos, Quaternion rot)
+    {
+        int pool = 0;
+        for (int p = 0; p < objectPools.Length; p++)
+        {
+            if (objectPools[p].poolName.Equals(poolName))
+            {
+                pool = p;
+                break;
+            }
+        }
+
+        for (int i = 0; i < objectPools[pool].pooledObjects.Count; i++)
+        {
+            if (!objectPools[pool].pooledObjects[i].activeInHierarchy)
+            {
+                GameObject obj = objectPools[pool].pooledObjects[i];
+                obj.transform.position = pos;
+                obj.transform.rotation = rot;
+                obj.SetActive(true);
+                return obj;
+            }
+        }
+        if (objectPools[pool].canResize)
+        {
+            GameObject obj = Instantiate(objectPools[pool].pooledObject, pos, rot);
+            objectPools[pool].pooledObjects.Add(obj);
+            objectPools[pool].poolSize++;
+            return obj;
+        }
+        return null;
+    }
+}
+
+
+ 
