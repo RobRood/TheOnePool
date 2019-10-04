1b31f57b0d947ce553031112faa783e20e4cc9a3
RobRood <therobertrood@gmail.com>
2019-10-04 11:49:18 -0700

    Update ObjectPool.cs

--- TheOnePool/ObjectPool.cs
+++ TheOnePool/ObjectPool.cs
@@ -14,7 +14,7 @@ public class ObjectPool
     public GameObject pooledObject;
     public int poolSize = 10;
     public bool canResize = true;
-
+    public PoolType poolType;
     public List<GameObject> pooledObjects;
     public string poolName; //optional, allows for pool to be referenced by name, first pool with matching name will be used
 }
@@ -30,28 +30,46 @@ public class ObjectPoolDrawer : PropertyDrawer
 
         position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
 
+        //var indent = EditorGUI.indentLevel;
+        //EditorGUI.indentLevel = 0;
+
         var nameField = new Rect(position.x - 40, position.y + 20, 150, 16);
         var objectField = new Rect(position.x - 40, position.y + 40, 150, 16);
         var sizeField = new Rect(position.x - 40, position.y + 60, 50, 16);
         var resizeField = new Rect(position.x + 80, position.y + 60, 30, 16);
+        var typeField = new Rect(position.x - 40, position.y + 80, 150, 16);
+
+
 
         EditorGUI.LabelField(new Rect(nameField.x - 86, nameField.y, 80, nameField.height), new GUIContent("Pool Name"));
-        EditorGUI.LabelField(new Rect(objectField.x - 86, objectField.y, 96, objectField.height), new GUIContent("Pooled Object"));
+        EditorGUI.LabelField(new Rect(objectField.x - 86, objectField.y, 98, objectField.height), new GUIContent("Pooled Object"));
         EditorGUI.LabelField(new Rect(sizeField.x - 86, sizeField.y, 80, sizeField.height), new GUIContent("Pool Size"));
-        EditorGUI.LabelField(new Rect(resizeField.x - 68, resizeField.y, 80, resizeField.height), new GUIContent("Can Resize"));
+        EditorGUI.LabelField(new Rect(resizeField.x - 72, resizeField.y, 84, resizeField.height), new GUIContent("Can Resize"));
+        EditorGUI.LabelField(new Rect(typeField.x - 86, typeField.y, 84, typeField.height), new GUIContent("Pool Type"));
 
 
         EditorGUI.PropertyField(nameField, property.FindPropertyRelative("poolName"), GUIContent.none);
         EditorGUI.PropertyField(objectField, property.FindPropertyRelative("pooledObject"), GUIContent.none);
         EditorGUI.PropertyField(sizeField, property.FindPropertyRelative("poolSize"), GUIContent.none);
-        EditorGUI.PropertyField(resizeField, property.FindPropertyRelative("canResize"), GUIContent.none); 
+        EditorGUI.PropertyField(resizeField, property.FindPropertyRelative("canResize"), GUIContent.none);
+        EditorGUI.PropertyField(typeField, property.FindPropertyRelative("poolType"), GUIContent.none);
 
+
+        //EditorGUI.indentLevel = indent;
         EditorGUI.EndProperty();
+        //return base.CreatePropertyGUI(property);
     }
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        return 90;
+        return 100;
     }
+}
+
+
+public enum PoolType
+{
+    GameObject, ParticleSystem
+
 }
 
